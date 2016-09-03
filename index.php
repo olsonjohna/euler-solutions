e9f1932a90244e91b65a560ebd1cf1cec09fa945
John Olson <john@homespotter.com>
2016-09-03 12:14:40 -0500

    Problem 18 solution.

--- index.php
+++ index.php
@@ -770,10 +770,85 @@
 		echo $totalLetters;
 	}
 
+	/*
+	By starting at the top of the triangle below and moving to adjacent numbers on the row below, the maximum 
+	total from top to bottom is 23.
+
+	3
+	7 4
+	2 4 6
+	8 5 9 3
+
+	That is, 3 + 7 + 4 + 9 = 23. Find the maximum total from top to bottom of the triangle below:
+
+	75
+	95 64
+	17 47 82
+	18 35 87 10
+	20 04 82 47 65
+	19 01 23 75 03 34
+	88 02 77 73 07 63 67
+	99 65 04 28 06 16 70 92
+	41 41 26 56 83 40 80 70 33
+	41 48 72 33 47 32 37 16 94 29
+	53 71 44 65 25 43 91 52 97 51 14
+	70 11 33 28 77 73 17 78 39 68 17 57
+	91 71 52 38 17 14 91 43 58 50 27 29 48
+	63 66 04 68 89 53 67 30 73 16 69 87 40 31
+	04 62 98 27 23 09 70 98 73 93 38 53 60 04 23
+	*/
 	function problem18() {
+		// Make triangle into array to traverse.
+		$valArray = array();
+		$valArray[] = array(75);
+		$valArray[] = array(95, 64);
+		$valArray[] = array(17, 47, 82);
+		$valArray[] = array(18, 35, 87, 10);
+		$valArray[] = array(20, 4, 82, 47, 65);
+		$valArray[] = array(19, 1, 23, 75, 3, 34);
+		$valArray[] = array(88, 2, 77, 73, 7, 63, 67);
+		$valArray[] = array(99, 65, 4, 28, 6, 16, 70, 92);
+		$valArray[] = array(41, 41, 26, 56, 83, 40, 80, 70, 33);
+		$valArray[] = array(41, 48, 72, 33, 47, 32, 37, 16, 94, 29);
+		$valArray[] = array(53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14);
+		$valArray[] = array(70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57);
+		$valArray[] = array(91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48);
+		$valArray[] = array(63, 66, 4, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31);
+		$valArray[] = array(4, 62, 98, 27, 23, 9, 70, 98, 73, 93, 38, 53, 60, 4, 23);
 
+		$sumArray = array();
+		// We need to add values from the original array for the first array. After that, we'll add the
+		// previous sum.
+		$useSumArray = false;
+
+		// Start at the second to last row of the array, minus 1 to get to last row, minus 1 to get to
+		// the row before the last row.
+		for ($i = count($valArray) - 2; $i >= 0; $i--) {
+			foreach ($valArray[$i] as $key => $value) {
+				// Sum the two possible values of the current value plus the two options from the row below.
+				$a = 0;
+				$b = 0;
+				if ($useSumArray) {
+					$a = $value + $sumArray[$i+1][$key];
+					$b = $value + $sumArray[$i+1][$key+1];
+				}
+				else {
+					$a = $value + $valArray[$i+1][$key];
+					$b = $value + $valArray[$i+1][$key+1];
+				}
+
+				// Add a value to the sum array of the greater of the two values.
+				if ($a > $b) {
+					$sumArray[$i][$key] = $a;
+				}
+				else {
+					$sumArray[$i][$key] = $b;
+				}
+			}
+			$useSumArray = true;
+		}
+		echo $sumArray[0][0];
 	}
-
 
 	/*
 	You are given the following information, but you may prefer to do some research for yourself.
