5a03a1751e49df4e9cfb69d92fe0a81b9effc522
John Olson <john@homespotter.com>
2016-09-04 18:46:09 -0500

    Bug fix for problem 1.

--- index.php
+++ index.php
@@ -20,14 +20,15 @@
 	these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000. 
 	*/
 	function problem1() {
-		// TODO: Perfomance.
 		$sum = 0;
-		$i = 1;
+		$i = 3;
 
 		while ($i < 1000) {
 			if ((($i % 3) == 0) || (($i % 5) == 0)) {
 				$sum = $sum + $i;
 			}
+
+			$i++;
 		}
 
 		echo $sum;
